a026a071c4870c35e2950034f15bf5296112425d
Bryan Sng <sngby98@gmail.com>
2019-06-18 16:20:53 +0100

    Updates

---
+++ precache-manifest.36c5e8ca0b743c1d2da30d5cd2b18454.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfcfce77ef22f4ec63157ec625c276fa",
+    "url": "/bryansng/potato-face-detector/index.html"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/bryansng/potato-face-detector/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "09146f0c5298d59ede96",
+    "url": "/bryansng/potato-face-detector/static/css/main.6f839502.chunk.css"
+  },
+  {
+    "revision": "86ba1a9d2b5b1bc023df",
+    "url": "/bryansng/potato-face-detector/static/js/2.7ca30bdf.chunk.js"
+  },
+  {
+    "revision": "09146f0c5298d59ede96",
+    "url": "/bryansng/potato-face-detector/static/js/main.c97d5865.chunk.js"
+  },
+  {
+    "revision": "8ecdc97df057bd219ecf",
+    "url": "/bryansng/potato-face-detector/static/js/runtime~main.665743ce.js"
+  },
+  {
+    "revision": "95de09b97dac3f6a1078ec565ef4bb8e",
+    "url": "/bryansng/potato-face-detector/static/media/brain.95de09b9.png"
+  }
+]);
